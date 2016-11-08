7a186fc32f7ddeef98a1febd599189d23fba0e80
wishket_yg <yonggill@wishket.com>
2016-11-08 22:19:51 +0900

    change license

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ setup(
     # package name
     name='ucloud-storage-python-wrapper',
     # package version
-    version='0.0.9',
+    version='0.1.1',
 
     py_modules=['uspw', ],
 
     # package struct
     packages=['uspw', ],
-    license='Apache 2.0',
+    license='GNU GPL 3.0',
 
     author='yonggill Lee',
     author_email='yonggill@wishket.com',
-    url='http://blog.yonggari.net',
+    url='https://www.wishket.com',
     description='python wrapper for KT ucloud storage service.',
     keywords=['kt', 'ucloud', 'storage', 'python', 'wrapper']
 )
